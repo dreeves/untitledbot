9edefd7af523e1553134b6f397fb4b524fd6450a
Jake Coble <jakecoble@users.noreply.github.com>
2021-01-21 14:14:21 -0500

    Autofocus the input box

--- client/chat.js
+++ client/chat.js
@@ -1,23 +1,26 @@
-const protocol = window.location.protocol === 'https' ? 'wss' : 'ws'
-const socket = new WebSocket(`${protocol}:${window.location.hostname}:${window.location.port}`)
-const messageForm = document.querySelector('#message-form')
-const messageInput = document.querySelector('#message-input')
+window.addEventListener('load', () => {
+    const protocol = window.location.protocol === 'https' ? 'wss' : 'ws'
+    const socket = new WebSocket(`${protocol}:${window.location.hostname}:${window.location.port}`)
+    const messageForm = document.querySelector('#message-form')
+    const messageInput = document.querySelector('#message-input')
 
-socket.addEventListener('open', () => {
-    messageInput.disabled = false
-})
-
-socket.addEventListener('message', e => {
-    const messageLine = document.createElement('li')
-    const messageLog = document.querySelector('#chat-log')
-
-    messageLine.textContent = e.data
-    messageLog.append(messageLine)
-})
-
-messageForm
-    .addEventListener('submit', e => {
-        e.preventDefault()
-        socket.send(messageInput.value)
-        messageInput.value = ''
+    socket.addEventListener('open', () => {
+        messageInput.disabled = false
+        messageInput.focus()
     })
+
+    socket.addEventListener('message', e => {
+        const messageLine = document.createElement('li')
+        const messageLog = document.querySelector('#chat-log')
+
+        messageLine.textContent = e.data
+        messageLog.append(messageLine)
+    })
+
+    messageForm
+        .addEventListener('submit', e => {
+            e.preventDefault()
+            socket.send(messageInput.value)
+            messageInput.value = ''
+        })
+})
